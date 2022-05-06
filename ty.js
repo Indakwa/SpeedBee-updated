bc41897534192d29a9d486b031225b54f2d506c6
Indakwa <benedictindakwa@gmail.com>
2022-05-07 00:50:30 +0300

    updated

---
+++ ty.js
@@ -0,0 +1,246 @@
+
+var text1Array = [
+    'After a year of retirement spent plopped on the couch bingeing on-demand TV and enjoying the freedom from corporate deadlines that filled sleepless nights with worry, I am thinking I better start setting my own deadlines and get my butt moving or else the inevitable deadline will arrive sooner than later', 
+    '“Thank you, Darling, that’s just exactly what I needed,” he said without even looking up, too busy shoveling the meal’s few delectable remnants into his drooling face; too insensitive and neglectful to provide the eye contact every great chef both craves and deserves.“You’re welcome,” she replied, mentally debating antidotes',
+
+    'Survival, the only thing that human beings strive to do, perhaps it should always be this way since all species believe they deserve a spot of existence. People lie, steal, bribe and so many other things just to survive. Somehow this act may be compared to a predator acting on a prey, because how else do we explain murder for survival if not the cycle of animal survival, that is the strongest get to live while the weak get to die. In a world where the winner always takes it all.',
+
+    'The picnic was everything they had hoped for. The table, beautifully set, sparkled in the afternoon sun. Heavenly aromas wafted on the summer breeze.They anxiously gathered around in anticipation of the upcoming feast.Betty brought potato salad.Bill made his famous meatloaf.Sadly for them, Mildred brought a fly swatter.',
+    'I once dated a man who never smelled. No sour morning breath, no corn-chip feet, no pungent sweat. My nose—less trusting than my heart—sniffed for signs of humanity. But even his gas did not provide a whiff. Farts of a hollow man, all sound and fury, signifying nothing',
+
+    'As the group approached the house, it was clearly uninhabited.The windows were dusty, cobwebs hung in every corner. We gonna do this or what? Jake asked the group. No one spoke.Creeping around back, even before gaining entry, they felt the presence.Emily said, I’m out! Everyone else died',
+
+    'Majestic waterfalls, cascading and splashing; deep rivers, surging and flowing; mountain streams, sparkling and burbling; heavy rain, falling and sweeping; powerful surf, cresting and churning; all God’s work, in which we rejoice, but all things Maxwell tried not to think about as he willed his excruciating bus ride to end.',
+    'He was doing headstands on the beach when a radio blared: Income inequality at all-time high!Sinking, he allowed the sand to reach his ears.Violence, poverty, global warming! “Oh, enough already,” he grumbled.Down went his head, nice and snug, until the radio was muted.Then the sea rose',
+
+    'Dough. I needed dough that day.Ray would loan it for a price.Me? I took the loan. "Oy vey-"father said,"such bad advice!" Soul? I sold mine on that day,lamenting the price I\'d pay.Teams of hitmen bat away \'til I pay them back their dough.',
+
+    'Soon after the admission to full membership of a significant number of female applicants, the board of a previously all-male Bostonian club considered a motion for a new construction project designed specifically to expand the capacity of the powder rooms.To no one’s great surprise, the voting followed potty lines.',
+    'On the way to the crematorium a large beer truck inserts itself between the hearse and the cortege, then, despite signals to overtake, tenaciously holds its position. In the moments of sad gladness, down the line of following cars, the laughter ripples. “Harry would have appreciated that,” they all agree.',
+
+    'Jukebox plays The Eagles. I nurse Amaretto Sours. One drawn-out sip, another. I absorb dim lights, make up stories. I’m a writer, pianist, actor. I join people in losing at pool, laugh over lost shots, forget bills, stacks of tests, and somnolent students. Laugh until dull stars lead me home',
+
+    'The flying saucer spiraled out of control. Hit a radio tower before ricocheting off a mountain spur. The dizzy aliens thought they were on their home planet, where they were called Ezees. They tried to explain this to paramedics, but were forever known as E.T.s. The acronym is a lie',
+
+    "The old man sat outside, whiskey in hand. Thoughts drifting to memories. That time, those stories, that other time, those other stories. He smiles. Memories of when whiskey wasn't trendy. Memories of a time when memories were only told through memories. His smile widens. Ah yes, memories of memories.",
+
+    'The octopus sat limp on the floor. Tentacles splayed across tiles. Martin looked at it, then, with a shrug of his shoulders, reached down, grabbed the octopus by it’s squishy head and tossed it back into the fish tank. He shook his head in bewilderment. Every damn day, he muttered',
+
+    'DECEASED: Ted Mickel, age 45. He leaves behind a loving wife, Molly. Ted served his family and country with conviction. He was an exemplary officer for the Orange county police department, who should not have eaten such a huge bite of prime rib without teaching his wife the Heimlich maneuver.',
+
+    'The kids gathered in a circle. The oldest was six. One by one, they each took turns. When everyone had finished their juice, they lay down, closed their eyes and waited for the spaceship to come take them to heaven. That was when Gina peed her pants, and ruined everything',
+
+    'Johnny traded his records for cassettes, then traded his cassettes for CDs, then traded his CDs for Mp3s. When his apartment flooded, his computer crashed and his old collections were ruined. He decided the only thing left to do was sing, play guitar, and make some music of his own.',
+
+    'It was an odd fetish; Mark paid her well for it.She entered the room, a pink human bunny.He spat on her, called her names.Her indulgence, his ecstasy.He got rough; she fell limp.How do you explain to your wife that you have just killed a hooker?',
+
+    'Riley smoked behind Walgreens. She named the local cat Miggles. She gave cigarettes to the bum near the dumpster. He had liquor breath; asked for food money. She never gave him any; he was a drunk. She hadn’t seen Miggles in awhile. The bum had stopped asking for food.',
+
+    "My tummy is growing. I have not eaten all day. There is food around the house, but I will not eat it. My body is but a vessel. My mind is strong. I am doing this to show that I have discipline, that I have faith. I feel kinda dizzy.",
+
+    'I believe that proper grammar and prose do not represent the only way to produce good writing. I believe that most readers do not know as much as we presume they know.I am firm in my beliefs.I am also a bitch who gets defensive when he is criticized',
+
+    'Golden-brown, and round. A crispy cylinder. Crunchy outside hiding bits of soft starchy goodness within. Can be dipped in ketchup, mayonnaise. Warm smelling. Piping hot inside. IF THEY DROP TO THE GROUND: DO NOT EAT THEM! You may get dirt in your mouth; they will not taste the same',
+
+    '"Hey man, you only left me a $3 tip.""So? You\'re a bartender, I ordered three rounds of drinks.""It was a fifty-five dollar tab.""So, you\'re calling me out? I tip a dollar per round.""Was the service that bad?""Was it that good?""Asshole.""Asshole."',
+
+    'Florescent lights and a humid breeze. Couples intertwined on sidewalks, against buildings, along a wall. High skirts, high bums, high hopes. An eddy of tourists and locals. Promoters, panhandlers, prostitutes. Taxis, bikes, the occasional muscle car. The smell of food, sweat and exhaust fumes. Blaring music. Fashion, flirting. Meandering masses.',
+
+    'The vending machine stuck. Fritos hung dangling from the coil. Shake, shake. The dollar insert slot was taped over. She was out of change, save for two nickels. Shake, shake. Shake, shake, shakeshakeshakeshake! A coworker walked by and slammed her face, nose first, into the machine. Blood gushed. Fritos fell.',
+
+    'This is by far the simplest text to type. Yeah, the simplest. No quotations, no crazy hyphens and abbreviations. It is just plain text with a few random numerals like 65, 90 and 1. I cannot help it, I must throw one or two pangrams here. Wait, do you know what a pangram is? It is a sentence that has all alphabets like The five boxing wizards jump quickly.',
+        ];
+
+
+
+
+let volumeUp = document.getElementById("volumeUp");
+let muteBtn = document.getElementById("muteBtn");
+let myAudio = document.getElementById('myAudio');
+
+volumeUp.addEventListener("click", () => {
+    volumeUp.classList.toggle("show")
+    muteBtn.classList.toggle("show")
+    myAudio.volume = 0;
+})
+
+muteBtn.addEventListener("click", () => {
+    muteBtn.classList.toggle("show")
+    volumeUp.classList.toggle("show")  
+    myAudio.volume = 1;
+})
+
+
+var text1 = text1Array[Math.floor(Math.random()*text1Array.length)];
+
+document.getElementById('text1').innerHTML = text1;
+var inputBox = document.getElementById("input");
+
+function check() {
+    input = inputBox.value;
+    if(input.normalize() === text1.normalize()){
+        clearInterval(int);
+        document.getElementById('modal-btn2').click();
+        remarks();
+    }else{
+        clearInterval(int);
+        document.getElementById('modal-btn3').click();
+        remarks2();
+    }
+}
+
+const btn = document.getElementById('startTimer');
+
+let [milliseconds,seconds,minutes,hours] = [0,0,0,0];
+let timerRef = document.querySelector('.timerDisplay');
+let int = null;
+
+
+btn.addEventListener('click', () => {
+
+    if(btn.innerText === 'START'){
+        btn.innerText = 'STOP';
+          
+         if(int !== null){
+               clearInterval(int);
+         }
+         int = setInterval(displayTimer, 10);
+         document.getElementById('input').focus();
+        }
+        if(btn.innerText === 'STOP'){
+           btn.addEventListener('click', () => {
+            //btn.innerText = 'Start';
+            //clearInterval(int);
+            check();
+           // [milliseconds,seconds,minutes,hours] = [0,0,0,0];
+            //timerRef.innerHTML = '00 : 00 : 00 : 000';
+           })
+        }
+    
+});
+
+
+document.body.onkeyup = function(e){
+    if(e.keyCode == 17){
+        btn.click();
+    }
+}
+
+
+/*document.getElementById('pauseTimer').addEventListener('click', () => {
+    clearInterval(int);
+});
+
+
+
+document.getElementById('resetTimer').addEventListener('click', () => {
+    clearInterval(int);
+    [milliseconds,seconds,minutes,hours] = [0,0,0,0];
+    timerRef.innerHTML = '00 : 00 : 00 : 000';
+});*/
+
+function displayTimer(){
+    milliseconds += 10;
+    if(milliseconds == 1000){
+        milliseconds = 0;
+        seconds++;
+        if(seconds == 60){
+            seconds = 0;
+            minutes++;
+            if(minutes == 60){
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;   
+
+    timerRef.innerHTML =  `${h} : ${m} : ${s} : ${ms}`;
+
+    timeTaken = `${h} : ${m} : ${s} : ${ms}`;
+}
+
+
+function textAreaAdjust(element){
+    element.style.height = "1px";
+    element.style.height = (25 + element.scrollHeight) + "px";
+}
+
+window.onload = function(){
+    document.getElementById('modal-btn').click();
+}
+
+
+nameBtn = document.getElementById('saveName-btn');
+nameBtn.disabled = true
+inputName = document.getElementById('nName');
+
+inputName.addEventListener("keyup", ()=> {
+    nickName = inputName.value;
+    if(nickName.length > 0){
+        nameBtn.disabled = false;
+    }else{
+        nameBtn.disabled = true;
+    }
+})
+
+function playAudio(){
+        document.getElementById('myAudio').play();
+        volumeUp.classList.add("show")
+}
+
+function reload(){
+    window.location.reload();
+}
+
+var modal2 = document.getElementById('modal-body-2');
+
+function remarks(){
+    if(minutes <= 1 && seconds <= 30){
+        modal2.innerHTML = 'Perfecto ' + nickName + '! you are a legend in typing';
+    }else if(minutes == 1 && seconds > 30 && seconds <= 40){
+        modal2.innerHTML = 'Superb ' + nickName + '! you are a grand master in typing';
+    }else if(minutes == 1 && seconds > 40 && seconds <= 50){
+        modal2.innerHTML =  'Wonderful ' + nickName + '! you are now a master in typing';
+    }else if(minutes == 2 && seconds == 0){
+        modal2.innerHTML =  'Marvelous ' + nickName + '! you are an expert in typing';
+    }else if(minutes == 2 && seconds > 0 && seconds <= 10){
+        modal2.innerHTML = 'Outstanding ' + nickName + '! you are a pro in typing';
+    }else if(minutes == 2 && seconds > 10 && seconds <= 20){
+        modal2.innerHTML = 'Good job ' + nickName + ', you are a novice in typing';
+    }else if(minutes >= 2 && seconds > 20){
+        modal2.innerHTML =  'Good trial ' + nickName + ', you are still a noob in typing';
+    }
+}
+
+var modal3 = document.getElementById('modal-body-3');
+
+var _difference = text1.length - 5;
+
+function remarks2(){
+    if(input.length >= _difference){
+        modal3.innerHTML = 'Well done ' + nickName + ', you took <br>' + timeTaken +  '<br> you missed a word or two though.';   
+    }else{
+        modal3.innerHTML = 'Oops! Ensure you type each and every letter, check what u might have missed.';
+    }
+}
+
+function _continue(){
+    if(int!==null){
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer,10);
+}
+
+function openHelp(){
+    document.getElementById('modal-btn4').click();
+}
+
+function openAbout(){
+    document.getElementById('modal-btn5').click();
+}
+
+
